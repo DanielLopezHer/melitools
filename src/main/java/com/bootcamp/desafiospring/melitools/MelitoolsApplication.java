577540f73e5656db875d506b80602ddf3d00ee4a
DanielLopezHer <daniel.dhernandez@mercadolibre.com.mx>
2021-05-05 08:19:18 -0500

    Commit inicial.

---
+++ src/main/java/com/bootcamp/desafiospring/melitools/MelitoolsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.desafiospring.melitools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelitoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelitoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/desafiospring/melitools/MelitoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.desafiospring.melitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelitoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
