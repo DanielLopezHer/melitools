f50ebef9a383f05386faab8ea522fdbbf6391521
DanielLopezHer <daniel.dhernandez@mercadolibre.com.mx>
2021-05-06 19:09:18 -0500

    Comentario en la configuración del singleton.

--- src/main/java/com/bootcamp/desafiospring/melitools/configuration/SingletonConfiguration.java
+++ src/main/java/com/bootcamp/desafiospring/melitools/configuration/SingletonConfiguration.java
@@ -13,6 +13,11 @@ public class SingletonConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SingletonConfiguration.class);
 
+    /**
+     * initializes the singleton in charge of the persistence of the objects and loads in memory the content of the
+     * files of the products, users and publications.
+     * @author Daniel Alejandro López Hernández
+     * @throws IOException if a file was not found.*/
     @PostConstruct
     private void loadSingleton() throws IOException {
         LOGGER.info("Se inicia el singleton de persistencia.");
